bd4024b1204748556f919ea1ec81fc072a6f2abe
jennifer hodge <jennifer.hodge@russdarrow.com>
2026-03-25 17:47:31 -0500

    building practice backend for project

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const productRoutes = require("./routes/productRoutes");
 
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database was successful!");
+  })
+  .catch((error) => {
+    console.log("Database connection has failed", error.message);
+  });
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the CC Corporate Collection API" });
@@ -10,6 +21,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-app.listen(3001, () => {
+app.listen(process.env.PORT, () => {
   console.log("Welcome to the CC Corporate Collection API");
 });
